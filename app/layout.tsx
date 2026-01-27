2da59f17ec7ef4cf79f6f7a1efe2492569a00921
masterrudy <rudy@armoredfresh.com>
2026-01-27 16:36:18 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,8 +44,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PFR2X0QFJ2"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PFR2X0QFJ2');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
-        <GoogleAnalytics />
         {children}
       </body>
     </html>
