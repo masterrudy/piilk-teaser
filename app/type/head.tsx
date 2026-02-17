763b13b178c75b698f59b251a5b5f646ddde8071
masterrudy <rudy@armoredfresh.com>
2026-02-17 18:57:11 -0500

    Update head.tsx

--- app/type/head.tsx
+++ app/type/head.tsx
@@ -1,8 +1,13 @@
+// app/type/head.tsx
 export default function Head() {
   return (
     <>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
       <link
         href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500;9..40,600;9..40,700&family=JetBrains+Mono:wght@400;500&display=swap"
         rel="stylesheet"
