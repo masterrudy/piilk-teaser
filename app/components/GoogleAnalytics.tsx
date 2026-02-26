fd63cab773ea72cf45ddfd2597c3f3f2e77ce40d
masterrudy <rudy@armoredfresh.com>
2026-02-26 15:17:33 -0500

    Update GoogleAnalytics.tsx

--- app/components/GoogleAnalytics.tsx
+++ app/components/GoogleAnalytics.tsx
@@ -1,42 +1,9 @@
 "use client";
-
 import Script from "next/script";
-import { useEffect } from "react";
-
-declare global {
-  interface Window {
-    dataLayer?: any[];
-    gtag?: (...args: any[]) => void;
-  }
-}
 
 const GA_MEASUREMENT_ID = "G-PFR2X0QFJ2";
 
-function shouldDebugMode() {
-  if (typeof window === "undefined") return false;
-  try {
-    return new URLSearchParams(window.location.search).get("debug_ga") === "1";
-  } catch {
-    return false;
-  }
-}
-
 export default function GoogleAnalytics() {
-  useEffect(() => {
-    window.dataLayer = window.dataLayer || [];
-    function gtag(...args: any[]) {
-      window.dataLayer!.push(args);
-    }
-    window.gtag = window.gtag || gtag;
-
-    const debug_mode = shouldDebugMode();
-
-    window.gtag("js", new Date());
-    window.gtag("config", GA_MEASUREMENT_ID, {
-      debug_mode,
-    });
-  }, []);
-
   return (
     <>
       <Script
@@ -47,7 +14,11 @@ export default function GoogleAnalytics() {
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
-          window.gtag = window.gtag || gtag;
+          window.gtag = gtag;
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}', {
+            debug_mode: new URLSearchParams(window.location.search).get('debug_ga') === '1'
+          });
         `}
       </Script>
     </>
